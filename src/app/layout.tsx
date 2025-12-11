47d730bd589dcc55b37174ebb32c666ced4fef9d
Levy Prado <levyprado28@gmail.com>
2025-12-10 20:22:11 -0400

    style(layout): add Space Grotesk font to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,14 @@
 import type { Metadata } from 'next'
-import { Inter_Tight } from 'next/font/google'
+import { Inter_Tight, Space_Grotesk } from 'next/font/google'
 import './globals.css'
 
 const interTight = Inter_Tight({
   variable: '--font-inter',
+  subsets: ['latin'],
+})
+
+const spaceGrotesk = Space_Grotesk({
+  variable: '--font-space-grotesk',
   subsets: ['latin'],
 })
 
@@ -19,7 +24,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body className={`${interTight.variable} antialiased`}>{children}</body>
+      <body
+        className={`${interTight.variable} ${spaceGrotesk.variable} relative isolate antialiased`}
+      >
+        {children}
+      </body>
     </html>
   )
 }
