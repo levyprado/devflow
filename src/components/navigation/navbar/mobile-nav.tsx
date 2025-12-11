9982ad66f8e575a3da97c87253a684fd8cd7b68a
Levy Prado <levyprado28@gmail.com>
2025-12-10 20:23:02 -0400

    feat(ui): create MobileNav dialog component

---
+++ src/components/navigation/navbar/mobile-nav.tsx
@@ -0,0 +1,46 @@
+'use client'
+
+import { cn } from '@/lib/utils'
+import { Dialog } from '@base-ui-components/react/dialog'
+import { Cancel01Icon, Menu01Icon } from '@hugeicons/core-free-icons'
+import { HugeiconsIcon } from '@hugeicons/react'
+import { useState } from 'react'
+
+export default function MobileNav() {
+  const [open, setOpen] = useState(false)
+
+  return (
+    <Dialog.Root open={open} onOpenChange={setOpen}>
+      <Dialog.Trigger className='relative flex size-6 items-center justify-center'>
+        <HugeiconsIcon
+          icon={Menu01Icon}
+          size={24}
+          strokeWidth={2}
+          className={cn(
+            'absolute top-1/2 left-1/2 -translate-1/2 transition-all duration-300 ease-out',
+            open ? 'scale-0 opacity-0' : 'scale-100 opacity-100',
+          )}
+        />
+        <HugeiconsIcon
+          icon={Cancel01Icon}
+          size={24}
+          strokeWidth={2}
+          className={cn(
+            'absolute top-1/2 left-1/2 -translate-1/2 transition-all duration-300 ease-out',
+            open ? 'scale-100 opacity-100' : 'scale-0 opacity-0',
+          )}
+        />
+      </Dialog.Trigger>
+      <Dialog.Portal>
+        <Dialog.Backdrop className='fixed inset-0 min-h-dvh bg-black opacity-20 transition-opacity duration-300 ease-out data-ending-style:opacity-0 data-starting-style:opacity-0 supports-[-webkit-touch-callout:none]:absolute dark:opacity-70' />
+        <Dialog.Viewport>
+          <Dialog.Popup className='fixed top-14 left-0 min-h-dvh w-[80%] max-w-2xs bg-card transition-all duration-300 ease-out data-ending-style:-translate-x-full data-starting-style:-translate-x-full'>
+            <Dialog.Title />
+            <Dialog.Description />
+            <Dialog.Close />
+          </Dialog.Popup>
+        </Dialog.Viewport>
+      </Dialog.Portal>
+    </Dialog.Root>
+  )
+}
