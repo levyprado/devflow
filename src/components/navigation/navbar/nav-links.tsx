6ef3dfb4e7ed66be08ed60d15d1ce7e3148e8822
Levy Prado <levyprado28@gmail.com>
2025-12-10 21:45:18 -0400

    refactor(nav-links): enable auto-closing dialog for mobile links

--- src/components/navigation/navbar/nav-links.tsx
+++ src/components/navigation/navbar/nav-links.tsx
@@ -2,28 +2,61 @@
 
 import { navbarLinks } from '@/lib/constants'
 import { cn } from '@/lib/utils'
+import { Dialog } from '@base-ui-components/react'
 import { HugeiconsIcon } from '@hugeicons/react'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 
-export default function NavLinks() {
+export default function NavLinks({
+  isMobileNav = false,
+}: {
+  isMobileNav?: boolean
+}) {
   const pathname = usePathname()
 
   return (
     <div className='flex w-full flex-col gap-6'>
-      {navbarLinks.map((item) => (
-        <Link
-          key={item.href}
-          href={item.href}
-          className={cn(
-            'flex items-center gap-4 rounded-xl p-4 text-lg font-medium',
-            pathname === item.href ? 'bg-primary' : 'bg-transparent',
-          )}
-        >
-          <HugeiconsIcon icon={item.icon} size={24} strokeWidth={2} />
-          {item.label}
-        </Link>
-      ))}
+      {navbarLinks.map((item) => {
+        const linkContent = (
+          <>
+            <HugeiconsIcon icon={item.icon} size={24} strokeWidth={2} />
+            {item.label}
+          </>
+        )
+
+        if (isMobileNav) {
+          return (
+            <Dialog.Close
+              key={item.href}
+              nativeButton={false}
+              render={
+                <Link
+                  href={item.href}
+                  className={cn(
+                    'flex items-center gap-4 rounded-xl p-4 text-lg font-medium',
+                    pathname === item.href ? 'bg-primary' : 'bg-transparent',
+                  )}
+                >
+                  {linkContent}
+                </Link>
+              }
+            />
+          )
+        } else {
+          return (
+            <Link
+              key={item.href}
+              href={item.href}
+              className={cn(
+                'flex items-center gap-4 rounded-xl p-4 text-lg font-medium',
+                pathname === item.href ? 'bg-primary' : 'bg-transparent',
+              )}
+            >
+              {linkContent}
+            </Link>
+          )
+        }
+      })}
     </div>
   )
 }
