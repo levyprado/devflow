cfb1630838fd76d66147174a7ea10cb921cb1937
Levy Prado <levyprado28@gmail.com>
2025-12-12 17:44:15 -0400

    style: add box-shadow to navbar and header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,7 +3,7 @@ import MobileNav from './navigation/mobile-nav'
 
 export default function Header() {
   return (
-    <header className='sticky top-0 left-0 z-10 flex h-(--mobile-header-height) w-full items-center justify-between bg-card px-5 lg:h-(--desktop-header-height) lg:px-6'>
+    <header className='sticky top-0 left-0 z-10 flex h-(--mobile-header-height) w-full items-center justify-between bg-card px-5 shadow-sm lg:h-(--desktop-header-height) lg:px-6'>
       <Logo />
 
       <div className='flex items-center'>
