311e638cac44e5b7a4b0cc608333218d06490865
Levy Prado <levyprado28@gmail.com>
2025-12-12 02:18:40 -0400

    fix: adjust mobile nav overflow for responsiveness and add auth buttons

--- src/components/navigation/mobile-nav.tsx
+++ src/components/navigation/mobile-nav.tsx
@@ -4,7 +4,9 @@ import { cn } from '@/lib/utils'
 import { Dialog } from '@base-ui-components/react/dialog'
 import { Cancel01Icon, Menu01Icon } from '@hugeicons/core-free-icons'
 import { HugeiconsIcon } from '@hugeicons/react'
+import Link from 'next/link'
 import { useState } from 'react'
+import Button from '../ui/button'
 import NavLinks from './nav-links'
 
 export default function MobileNav() {
@@ -36,14 +38,30 @@ export default function MobileNav() {
       <Dialog.Portal className='sm:hidden'>
         <Dialog.Backdrop className='fixed inset-0 bg-black opacity-20 transition-opacity duration-300 ease-out data-ending-style:opacity-0 data-starting-style:opacity-0 supports-[-webkit-touch-callout:none]:absolute dark:opacity-70' />
         <Dialog.Viewport>
-          <Dialog.Popup className='fixed top-(--mobile-header-height) left-0 flex min-h-[calc(100dvh-var(--mobile-header-height))] w-[80%] max-w-2xs flex-col bg-card transition-all duration-300 ease-out data-ending-style:-translate-x-full data-starting-style:-translate-x-full'>
-            <nav className='flex flex-1 flex-col items-start justify-between px-5 pt-12 pb-10'>
+          <Dialog.Popup
+            render={<nav />}
+            className='fixed top-(--mobile-header-height) left-0 flex h-[calc(100dvh-var(--mobile-header-height))] w-[80%] max-w-2xs flex-col gap-6 bg-card px-5 pt-12 pb-10 transition-all duration-300 ease-out data-ending-style:-translate-x-full data-starting-style:-translate-x-full'
+          >
+            <div className='flex-1 overflow-y-auto overscroll-contain'>
               <NavLinks isMobileNav={true} />
+            </div>
 
-              <div className='flex flex-col'>
-                {/* Authentication buttons */}
-              </div>
-            </nav>
+            <div className='flex flex-col gap-3'>
+              <Button
+                render={<Link href='/login' />}
+                nativeButton={false}
+                variant='outline'
+              >
+                Log in
+              </Button>
+              <Button
+                render={<Link href='/signup' />}
+                nativeButton={false}
+                variant='outline'
+              >
+                Sign up
+              </Button>
+            </div>
           </Dialog.Popup>
         </Dialog.Viewport>
       </Dialog.Portal>
