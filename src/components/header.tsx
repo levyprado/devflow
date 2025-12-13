583ee2bd1e68a57849d3098fee7cf6df403bb228
Levy Prado <levyprado28@gmail.com>
2025-12-13 17:15:26 -0400

    feat: add global search bar to header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,16 +3,32 @@ import MobileNav from './navigation/mobile-nav'
 
 export default function Header() {
   return (
-    <header className='sticky top-0 left-0 z-10 flex h-(--mobile-header-height) w-full items-center justify-between bg-card px-5 shadow-sm lg:h-(--desktop-header-height) lg:px-6'>
-      <Logo />
+    <>
+      <header className='sticky top-0 left-0 z-10 flex h-(--mobile-header-height) w-full items-center justify-between bg-card px-5 shadow-sm lg:h-(--desktop-header-height) lg:px-6'>
+        <Logo />
 
-      <div className='flex items-center'>
-        {/* Theme Toggle */}
+        <div className='hidden sm:block'>
+          <input
+            type='search'
+            placeholder='Search anything globally'
+            className='rounded-xl border bg-card px-4 py-2'
+          />
+        </div>
 
-        {/* Profile */}
+        <div className='flex items-center'>
+          {/* Theme Toggle */}
+          {/* Profile */}
+          <MobileNav />
+        </div>
+      </header>
 
-        <MobileNav />
+      <div className='flex w-full flex-col bg-card px-5 pb-5 sm:hidden'>
+        <input
+          type='search'
+          placeholder='Search anything globally'
+          className='rounded-xl border bg-card px-4 py-2'
+        />
       </div>
-    </header>
+    </>
   )
 }
