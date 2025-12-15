6a09021ed9f18c39b7895158c02277a88dddfdd7
Levy Prado <levyprado28@gmail.com>
2025-12-15 17:11:36 -0400

    refactor: replace inline search bar with search input component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,18 +1,15 @@
 import Logo from './logo'
 import MobileNav from './navigation/mobile-nav'
+import SearchInput from './search-input'
 
 export default function Header() {
   return (
     <>
-      <header className='sticky top-0 left-0 z-10 flex h-(--mobile-header-height) w-full items-center justify-between bg-card px-5 shadow-sm lg:h-(--desktop-header-height) lg:px-6'>
+      <header className='sticky top-0 left-0 z-10 flex h-(--mobile-header-height) w-full items-center justify-between gap-16 bg-card px-5 sm:shadow-sm lg:h-(--desktop-header-height) lg:px-6'>
         <Logo />
 
-        <div className='hidden sm:block'>
-          <input
-            type='search'
-            placeholder='Search anything globally'
-            className='rounded-xl border bg-card px-4 py-2'
-          />
+        <div className='hidden sm:block sm:w-full'>
+          <SearchInput />
         </div>
 
         <div className='flex items-center'>
@@ -22,12 +19,8 @@ export default function Header() {
         </div>
       </header>
 
-      <div className='flex w-full flex-col bg-card px-5 pb-5 sm:hidden'>
-        <input
-          type='search'
-          placeholder='Search anything globally'
-          className='rounded-xl border bg-card px-4 py-2'
-        />
+      <div className='flex w-full flex-col bg-card px-5 pt-0.5 pb-5 shadow-sm sm:hidden'>
+        <SearchInput />
       </div>
     </>
   )
