f077550ad715b9c044fab180b4b113e828d567ec
Levy Prado <levyprado28@gmail.com>
2025-12-16 23:15:28 -0400

    feat: add dynamic routes constants for profiles, questions, and tags

--- src/lib/constants.tsx
+++ src/lib/constants.tsx
@@ -57,4 +57,7 @@ export const ROUTES = {
   HOME: '/',
   SIGN_IN: '/sign-in',
   SIGN_UP: '/sign-up',
+  PROFILE: (id: string) => `/profile/${id}`,
+  QUESTION: (id: string) => `/questions/${id}`,
+  TAGS: (id: string) => `/tags/${id}`,
 }
